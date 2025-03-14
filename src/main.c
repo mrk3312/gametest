752f116f7c3deba3352778d768499c403c72fd1d
mrk3312 <168173378+mrk3312@users.noreply.github.com>
2025-03-14 03:49:43 +0200

    Source code

---
+++ src/main.c
@@ -0,0 +1,135 @@
+#define RAYGUI_IMPLEMENTATION
+#include "game.h"
+
+Cell grid[COLS][ROWS];
+
+bool inMenu = false;
+bool justExitedMenu = true;
+
+bool turnShow = true;
+unsigned int turn = 0;
+
+unsigned short int numberWindGenerator = 0;
+unsigned short int numberSolarPanel = 0;
+
+float money = 10000;
+float priceCell = 1000;
+WeatherCondition randomWeather = SUNNY;
+unsigned short int randomWind = 15;
+
+int constructionCapabilities = 1000;
+int usedConstructionCapabilities = 1000;
+
+int main(){
+
+    GameScreen screen = GAMEPLAY;
+    const int screenWidth = 800;
+    const int screenHeight = 800;
+    int cellWidth = screenHeight / COLS;
+    int cellHeight = screenWidth / ROWS;
+    
+    InitWindow(screenWidth, screenHeight, "Debug");
+
+    for(int i = 0; i < COLS; i++)
+    {
+        for(int j = 0; j < ROWS; j++)
+        {
+            grid[i][j] = (Cell) 
+            {
+                .i = i,
+                .j = j,
+                .isSelected = false,
+                .isInactive = (j == 0 || j == 1),
+                .isUnlocked = (i > 6 && i < 9 && j > 7 && j < 10),
+                .building = EMPTY,
+                .constructionUpgradingStatus = NOTUPGRADING,
+                .efficiencyLevel = LEVEL00,
+                .reliabilityLevel = LEVEL00,
+                .turnsConstructing = 0,
+            };
+        }
+    }
+
+    //Main while loop
+    while(!WindowShouldClose()) 
+    {   
+
+        CalculateEntities();
+        Vector2 mousePosition = GetMousePosition();
+        static int indexI;
+        static int indexJ;
+        
+        if(IsKeyPressed(KEY_ENTER))
+        {
+            switch(screen)
+            {
+                case(MAINMENU):
+                {
+                    screen = GAMEPLAY;
+                    break;
+                }
+
+                case(GAMEPLAY):
+                {
+                    screen = MAINMENU;
+                    break;
+                }
+
+            }
+        }
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !inMenu)
+        {
+            indexI = CalculateMousePosition(mousePosition.x, cellWidth);
+            indexJ = CalculateMousePosition(mousePosition.y, cellHeight);
+            if(IsIndexValid(indexI, indexJ) && !inMenu && !grid[indexI][indexJ].isInactive && !IsBorderingLockedCellsOnly(indexI, indexJ))
+            {
+                    EnterCellMenu(indexI, indexJ);
+            }
+        } 
+        
+            BeginDrawing();
+
+            switch(screen)
+            {
+
+                case MAINMENU:
+                {
+                    DrawRectangle(0, 0, 800, 800, RED);
+                    break;
+                };
+
+                case GAMEPLAY:
+                {
+                    ButtonMenu();
+                    if(turnShow)
+                    {
+                        DrawText(TextFormat("Week: %d", turn), 325, 0, 40, GRAY);
+                        DrawText(TextFormat("$%.2f", money), 0, 0, 40, GREEN);
+                        DrawText(TextFormat("%d / %d", usedConstructionCapabilities, constructionCapabilities), 550, 0, 40, GRAY);
+                    }
+
+                    for(int i = 0; i < COLS; i++)
+                    {
+                        for(int j = 0; j < ROWS; j++)
+                        {
+                            CellPaint(i, j, cellWidth, cellHeight);
+                            CellDraw(i, j, cellWidth, cellHeight);
+                        }
+                    }
+                    if(grid[indexI][indexJ].isSelected)
+                        CellMenu(indexI, indexJ);
+                }  
+                break;
+            }
+
+            ClearBackground(RAYWHITE);
+            EndDrawing();
+        
+    }
+
+    CloseWindow();
+    return 0;
+
+}
+
+
